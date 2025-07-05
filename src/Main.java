d999d367a735bb41c9698842629e36c7d51583c5
YassoKhalid <153525555+YassoKhalid@users.noreply.github.com>
2025-07-05 06:13:05 +0300

    Edited some names

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,11 @@ import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
-        Customer c = new Customer("Ship", "s@example.com", "01000000003", 200000000);
+        Customer c = new Customer("Ship", "yassenkhaled927@gmail.com", "01287663795", 200000000);
         Cart cart = new Cart();
         LocalDate date = LocalDate.now();
         date = date.plusDays(1);
-        Cheese ch = new Cheese("Gebna", 20.0, 5, date, 550.0);
+        Cheese chees = new Cheese("Gebna", 20.0, 5, date, 550.0);
         Mobile mb = new Mobile("Samasung S20", 1500, 20, "Samaung", "Galaxy", "White");
         Biscuit biscuit = new Biscuit("Fairy", 10.0, 20, "Egypt Foods", date);
         TV tv = new TV("SamSmart", 28000, 150000, 10, 15000);
