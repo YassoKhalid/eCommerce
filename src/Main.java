a86de63d1d3cd3bb47ddf1118ebdee24a8ad6453
YassoKhalid <153525555+YassoKhalid@users.noreply.github.com>
2025-07-05 06:24:53 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,18 @@ import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
-        Customer c = new Customer("Ship", "s@example.com", "01000000003", 200000000);
+        Customer c = new Customer("Ship", "yassenkhaled927@gmail.com", "01287663795", 200000000);
         Cart cart = new Cart();
         LocalDate date = LocalDate.now();
         date = date.plusDays(1);
-        Cheese ch = new Cheese("Gebna", 20.0, 5, date, 550.0);
+        Cheese chees = new Cheese("Gebna", 20.0, 5, date, 550.0);
         Mobile mb = new Mobile("Samasung S20", 1500, 20, "Samaung", "Galaxy", "White");
-        cart.addCartItem(mb, 10);
-        cart.addCartItem(new cartItem(ch, 4));
         Biscuit biscuit = new Biscuit("Fairy", 10.0, 20, "Egypt Foods", date);
+        TV tv = new TV("SamSmart", 28000, 150000, 10, 15000);
         cart.addCartItem(biscuit, 10);
+        cart.addCartItem(mb, 10);
+        cart.addCartItem(new cartItem(chees, 4));
+        cart.addCartItem(new cartItem(tv, 2));
         cart.Checkout(c);
     }
 }
