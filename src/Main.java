dd6e8ce77bc32867be3d1c5a705e24dae014f7a6
YassoKhalid <153525555+YassoKhalid@users.noreply.github.com>
2025-07-05 06:18:55 +0300

    Revert "Merge pull request #3"
    This reverts commit cc36e0a65996f410ed4144480398f04df627f8ce.

--- src/Main.java
+++ src/Main.java
@@ -8,12 +8,10 @@ public class Main {
         date = date.plusDays(1);
         Cheese ch = new Cheese("Gebna", 20.0, 5, date, 550.0);
         Mobile mb = new Mobile("Samasung S20", 1500, 20, "Samaung", "Galaxy", "White");
-        Biscuit biscuit = new Biscuit("Fairy", 10.0, 20, "Egypt Foods", date);
-        TV tv = new TV("SamSmart", 28000, 150000, 10, 15000);
-        cart.addCartItem(biscuit, 10);
         cart.addCartItem(mb, 10);
         cart.addCartItem(new cartItem(ch, 4));
-        cart.addCartItem(new cartItem(tv, 2));
+        Biscuit biscuit = new Biscuit("Fairy", 10.0, 20, "Egypt Foods", date);
+        cart.addCartItem(biscuit, 10);
         cart.Checkout(c);
     }
 }
